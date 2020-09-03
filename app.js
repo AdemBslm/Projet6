ba560e36dfc14d1d136c4357cd969fa5e831014c
Adem <tpegrp6132@gmail.com>
2020-09-03 15:16:43 +0200

    Debut du code pour les user

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://adem29:adem13200@cluster0.dseuq.mongodb.net/<dbname>?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use((req, res) => {
+    res.json({ message: 'Votre requête a bien été reçue !!' }); 
+});
+
+module.exports = app;
